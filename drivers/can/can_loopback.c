4669a2f139c79da24d78dc80731918523e6bab76
Giuseppe Iellamo <giuseppe.iellamo@swhard.it>
2020-04-09 13:50:09 +0200

    [DEV] Dual Loopback Xconnected

--- drivers/can/can_loopback.c
+++ drivers/can/can_loopback.c
@@ -17,6 +17,12 @@ K_THREAD_STACK_DEFINE(tx_thread_stack,
 		      CONFIG_CAN_LOOPBACK_TX_THREAD_STACK_SIZE);
 struct k_thread tx_thread_data;
 
+K_THREAD_STACK_DEFINE(tx_thread_stack2,
+		      CONFIG_CAN_LOOPBACK_TX_THREAD_STACK_SIZE);
+struct k_thread tx_thread_data2;
+
+static int thread_count = 0;
+
 struct can_looppback_frame {
 	struct zcan_frame frame;
 	can_tx_callback_t cb;
@@ -24,7 +30,10 @@ struct can_looppback_frame {
 	struct k_sem *tx_compl;
 };
 
-K_MSGQ_DEFINE(tx_msgq, sizeof(struct can_looppback_frame),
+K_MSGQ_DEFINE(tx_msgq_0, sizeof(struct can_looppback_frame),
+	      CONFIG_CAN_LOOPBACK_TX_MSGQ_SIZE, 4);
+
+K_MSGQ_DEFINE(tx_msgq_1, sizeof(struct can_looppback_frame),
 	      CONFIG_CAN_LOOPBACK_TX_MSGQ_SIZE, 4);
 
 static void dispatch_frame(const struct zcan_frame *frame,
@@ -32,12 +41,11 @@ static void dispatch_frame(const struct zcan_frame *frame,
 {
 	struct zcan_frame frame_tmp = *frame;
 
-	LOG_DBG("Receiving %d bytes. Id: 0x%x, ID type: %s %s",
-		frame->dlc,
-		frame->id_type == CAN_STANDARD_IDENTIFIER ?
-				  frame->std_id : frame->ext_id,
-		frame->id_type == CAN_STANDARD_IDENTIFIER ?
-				  "standard" : "extended",
+	LOG_INF("Receiving %d bytes. Id: 0x%x, ID type: %s %s", frame->dlc,
+		frame->id_type == CAN_STANDARD_IDENTIFIER ? frame->std_id :
+							    frame->ext_id,
+		frame->id_type == CAN_STANDARD_IDENTIFIER ? "standard" :
+							    "extended",
 		frame->rtr == CAN_DATAFRAME ? "" : ", RTR frame");
 
 	filter->rx_cb(&frame_tmp, filter->cb_arg);
@@ -48,12 +56,13 @@ static inline int check_filter_match(const struct zcan_frame *frame,
 {
 	u32_t id, mask, frame_id;
 
-	frame_id = frame->id_type == CAN_STANDARD_IDENTIFIER ?
-			frame->std_id : frame->ext_id;
-	id = filter->id_type == CAN_STANDARD_IDENTIFIER ?
-			filter->std_id : filter->ext_id;
+	frame_id = frame->id_type == CAN_STANDARD_IDENTIFIER ? frame->std_id :
+							       frame->ext_id;
+	id = filter->id_type == CAN_STANDARD_IDENTIFIER ? filter->std_id :
+							  filter->ext_id;
 	mask = filter->id_type == CAN_STANDARD_IDENTIFIER ?
-			filter->std_id_mask : filter->ext_id_mask;
+		       filter->std_id_mask :
+		       filter->ext_id_mask;
 
 	return ((id & mask) == (frame_id & mask));
 }
@@ -66,14 +75,17 @@ void tx_thread(void *data_arg, void *arg2, void *arg3)
 	struct can_loopback_filter *filter;
 	struct can_loopback_data *data = (struct can_loopback_data *)data_arg;
 
+	struct k_msgq *tx_msgq = data->id == 0 ? &tx_msgq_0 : &tx_msgq_1;
+
 	while (1) {
-		k_msgq_get(&tx_msgq, &frame, K_FOREVER);
+		k_msgq_get(tx_msgq, &frame, K_FOREVER);
 		k_mutex_lock(&data->mtx, K_FOREVER);
 
 		for (int i = 0; i < CONFIG_CAN_MAX_FILTER; i++) {
 			filter = &data->filters[i];
 			if (filter->rx_cb &&
 			    check_filter_match(&frame.frame, &filter->filter)) {
+				LOG_INF(" dispatch of id %d \n", data->id);
 				dispatch_frame(&frame.frame, filter);
 			}
 		}
@@ -97,21 +109,18 @@ int can_loopback_send(struct device *dev, const struct zcan_frame *frame,
 	struct can_looppback_frame loopback_frame;
 	struct k_sem tx_sem;
 
-	LOG_DBG("Sending %d bytes on %s. Id: 0x%x, ID type: %s %s",
-		frame->dlc, dev->config->name,
-		frame->id_type == CAN_STANDARD_IDENTIFIER ?
-				  frame->std_id : frame->ext_id,
-		frame->id_type == CAN_STANDARD_IDENTIFIER ?
-				  "standard" : "extended",
+	LOG_INF("Sending %d bytes on %s. Id: 0x%x, ID type: %s %s", frame->dlc,
+		dev->config->name,
+		frame->id_type == CAN_STANDARD_IDENTIFIER ? frame->std_id :
+							    frame->ext_id,
+		frame->id_type == CAN_STANDARD_IDENTIFIER ? "standard" :
+							    "extended",
 		frame->rtr == CAN_DATAFRAME ? "" : ", RTR frame");
 
 	if (frame->dlc > CAN_MAX_DLC) {
-		LOG_ERR("DLC of %d exceeds maximum (%d)", frame->dlc, CAN_MAX_DLC);
+		LOG_ERR("DLC of %d exceeds maximum (%d)", frame->dlc,
+			CAN_MAX_DLC);
 		return CAN_TX_EINVAL;
-	}
-
-	if (!data->loopback) {
-		return 0;
 	}
 
 	loopback_frame.frame = *frame;
@@ -123,15 +132,19 @@ int can_loopback_send(struct device *dev, const struct zcan_frame *frame,
 		k_sem_init(&tx_sem, 0, 1);
 	}
 
-	ret = k_msgq_put(&tx_msgq, &loopback_frame, timeout);
+#if (defined CONFIG_CAN_1 && defined CONFIG_CAN_0)
+	struct k_msgq *tx_msgq = data->id == 0 ? &tx_msgq_1 : &tx_msgq_0;
+#else
+	struct k_msgq *tx_msgq = data->id == 0 ? &tx_msgq_0 : &tx_msgq_1;
+#endif
+	ret = k_msgq_put(tx_msgq, &loopback_frame, timeout);
 
 	if (!callback) {
 		k_sem_take(&tx_sem, K_FOREVER);
 	}
 
-	return  ret ? CAN_TIMEOUT : CAN_TX_OK;
+	return ret ? CAN_TIMEOUT : CAN_TX_OK;
 }
-
 
 static inline int get_free_filter(struct can_loopback_filter *filters)
 {
@@ -145,21 +158,22 @@ static inline int get_free_filter(struct can_loopback_filter *filters)
 }
 
 int can_loopback_attach_isr(struct device *dev, can_rx_callback_t isr,
-			    void *cb_arg,
-			    const struct zcan_filter *filter)
+			    void *cb_arg, const struct zcan_filter *filter)
 {
 	struct can_loopback_data *data = DEV_DATA(dev);
 	struct can_loopback_filter *loopback_filter;
 	int filter_id;
 
 	LOG_DBG("Setting filter ID: 0x%x, mask: 0x%x", filter->ext_id,
-		    filter->ext_id_mask);
+		filter->ext_id_mask);
 	LOG_DBG("Filter type: %s ID %s mask",
-		    filter->id_type == CAN_STANDARD_IDENTIFIER ?
-			"standard" : "extended",
-		     ((filter->id_type && (filter->std_id_mask == CAN_STD_ID_MASK)) ||
-		     (!filter->id_type && (filter->ext_id_mask == CAN_EXT_ID_MASK))) ?
-			"with" : "without");
+		filter->id_type == CAN_STANDARD_IDENTIFIER ? "standard" :
+							     "extended",
+		((filter->id_type && (filter->std_id_mask == CAN_STD_ID_MASK)) ||
+		 (!filter->id_type &&
+		  (filter->ext_id_mask == CAN_EXT_ID_MASK))) ?
+			"with" :
+			"without");
 
 	k_mutex_lock(&data->mtx, K_FOREVER);
 	filter_id = get_free_filter(data->filters);
@@ -193,7 +207,7 @@ void can_loopback_detach(struct device *dev, int filter_id)
 }
 
 int can_loopback_configure(struct device *dev, enum can_mode mode,
-				u32_t bitrate)
+			   u32_t bitrate)
 {
 	struct can_loopback_data *data = DEV_DATA(dev);
 
@@ -245,11 +259,12 @@ static const struct can_driver_api can_api_funcs = {
 	.register_state_change_isr = can_loopback_register_state_change_isr
 };
 
-
 static int can_loopback_init(struct device *dev)
 {
 	struct can_loopback_data *data = DEV_DATA(dev);
 	k_tid_t tx_tid;
+
+	LOG_INF("Init of %s", dev->config->name);
 
 	k_mutex_init(&data->mtx);
 
@@ -257,30 +272,51 @@ static int can_loopback_init(struct device *dev)
 		data->filters[i].rx_cb = NULL;
 	}
 
-	tx_tid = k_thread_create(&tx_thread_data, tx_thread_stack,
-				 K_THREAD_STACK_SIZEOF(tx_thread_stack),
-				 tx_thread, data, NULL, NULL,
-				 CONFIG_CAN_LOOPBACK_TX_THREAD_PRIORITY,
-				 0, K_NO_WAIT);
+	if (thread_count == 0) {
+		tx_tid = k_thread_create(&tx_thread_data, tx_thread_stack,
+					 K_THREAD_STACK_SIZEOF(tx_thread_stack),
+					 tx_thread, data, NULL, NULL,
+					 CONFIG_CAN_LOOPBACK_TX_THREAD_PRIORITY,
+					 0, K_NO_WAIT);
+		data->id = 0;
+	} else if (thread_count == 1) {
+		tx_tid =
+			k_thread_create(&tx_thread_data2, tx_thread_stack2,
+					K_THREAD_STACK_SIZEOF(tx_thread_stack2),
+					tx_thread, data, NULL, NULL,
+					CONFIG_CAN_LOOPBACK_TX_THREAD_PRIORITY,
+					0, K_NO_WAIT);
+		data->id = 1;
+	}
 	if (!tx_tid) {
 		LOG_ERR("ERROR spawning tx thread");
 		return -1;
 	}
+	thread_count++;
 
 	LOG_INF("Init of %s done", dev->config->name);
 	return 0;
 }
+
+//#define CONFIG_CAN_0
+//#define CONFIG_CAN_1
+
+#ifdef CONFIG_CAN_0
+static struct can_loopback_data can_loopback_dev_data_0;
+
+DEVICE_AND_API_INIT(can_loopback_0, "CAN_LOOP_0", &can_loopback_init,
+		    &can_loopback_dev_data_0, NULL, POST_KERNEL,
+		    CONFIG_KERNEL_INIT_PRIORITY_DEVICE, &can_api_funcs);
+#endif
 
 #ifdef CONFIG_CAN_1
 
 static struct can_loopback_data can_loopback_dev_data_1;
 
 DEVICE_AND_API_INIT(can_loopback_1, CONFIG_CAN_LOOPBACK_DEV_NAME,
-		    &can_loopback_init,
-		    &can_loopback_dev_data_1, NULL,
+		    &can_loopback_init, &can_loopback_dev_data_1, NULL,
 		    POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
 		    &can_api_funcs);
-
 
 #if defined(CONFIG_NET_SOCKETS_CAN)
 
@@ -291,26 +327,23 @@ static int socket_can_init_1(struct device *dev)
 	struct device *can_dev = DEVICE_GET(can_loopback_1);
 	struct socket_can_context *socket_context = dev->driver_data;
 
-	LOG_DBG("Init socket CAN device %p (%s) for dev %p (%s)",
-		dev, dev->config->name, can_dev, can_dev->config->name);
+	LOG_DBG("Init socket CAN device %p (%s) for dev %p (%s)", dev,
+		dev->config->name, can_dev, can_dev->config->name);
 
 	socket_context->can_dev = can_dev;
 	socket_context->msgq = &socket_can_msgq;
 
-	socket_context->rx_tid =
-		k_thread_create(&socket_context->rx_thread_data,
-				rx_thread_stack,
-				K_THREAD_STACK_SIZEOF(rx_thread_stack),
-				rx_thread, socket_context, NULL, NULL,
-				RX_THREAD_PRIORITY, 0, K_NO_WAIT);
+	socket_context->rx_tid = k_thread_create(
+		&socket_context->rx_thread_data, rx_thread_stack,
+		K_THREAD_STACK_SIZEOF(rx_thread_stack), rx_thread,
+		socket_context, NULL, NULL, RX_THREAD_PRIORITY, 0, K_NO_WAIT);
 
 	return 0;
 }
 
 NET_DEVICE_INIT(socket_can_loopback_1, SOCKET_CAN_NAME_1, socket_can_init_1,
 		device_pm_control_nop, &socket_can_context_1, NULL,
-		CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
-		&socket_can_api,
+		CONFIG_KERNEL_INIT_PRIORITY_DEVICE, &socket_can_api,
 		CANBUS_RAW_L2, NET_L2_GET_CTX_TYPE(CANBUS_RAW_L2), CAN_MTU);
 
 #endif /* CONFIG_NET_SOCKETS_CAN */
